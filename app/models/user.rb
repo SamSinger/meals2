d1d1bd92978fa78845f53ddeba1daedbe0c6051d
Sam Singer <samsingerjr@gmail.com>
2015-03-25 15:43:51 -0500

    fixed a problem with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :service_assignments
-  has_many :users, :through => :service_assignments
+  has_many :services, :through => :service_assignments
   
   validates_presence_of :email, :password, :firstname, :lastname, :organization_name
   validates_uniqueness_of :email
