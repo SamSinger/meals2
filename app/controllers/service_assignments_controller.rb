1608dddc3a2bc80ab7f19392f6b2889490de522b
Sam Singer <samsingerjr@gmail.com>
2015-04-30 09:39:58 -0500

    corrected typo on service_assignments_controller.rb

--- app/controllers/service_assignments_controller.rb
+++ app/controllers/service_assignments_controller.rb
@@ -2,7 +2,7 @@ class ServiceAssignmentsController < ApplicationController
   def create
     service_assignment_params[:service_assignment].select{|key, value| value[:taken] == "1" || value[:id].present? }.each do |key, value|
       if value[:id].present?        
-        ServiceAssignment.find(value[:id]).destroy  
+        ServiceAssignment.find(value[:id]).destroy if value[:taken] == "0" 
       else
         ServiceAssignment.create(service_id: value[:service_id], assignment_date: value[:assignment_date], user_id: current_user.id)
       end
